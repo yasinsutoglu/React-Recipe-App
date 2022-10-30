0519ce8c86432d7220ffda7bc6452100ba0d941d
Yasin Sutoglu <ysutoglu@gmail.com>
2022-10-30 15:15:32 +0300

    about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,9 +9,9 @@ const About = () => {
       <Navbar />
         <AboutDiv>
           <AboutImage src={coding} />
-          <AboutTitle>About Software Developer <span>Ali Aksoy</span></AboutTitle>
+          <AboutTitle>About Software Developer <span>Yasin Sutoglu</span></AboutTitle>
           <AboutDetailDiv>
-            <h2>I'm Ali</h2>
+            <h2>I'm Yasin</h2>
             <p style={{fontSize:"1rem", fontWeight:"600"}}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
             <p style={{fontSize:"1rem", fontWeight:"600"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, quibusdam?</p>
             <p style={{fontSize:"1rem", fontWeight:"600"}}><span  style={{textDecoration:"underline", color:"white" ,fontSize:"1.1rem"}}>Send email:</span> Loremipsum71@dolor.com</p>
